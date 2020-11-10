146a5a3bbef16ac110e18b9481da64bcc7d91164
LeoCCR <private@private.com>
2020-11-10 22:24:10 +0800

    Extract data from TPM.csv file.

--- analysis.py
+++ analysis.py
@@ -2,16 +2,22 @@
 import csv
 from list_csv import list_csv
 
-try_path = "C:\\Users\\hwligroup\\Desktop\\lane 1-1.csv"
+try_path = "/Users/renxmac/Desktop/lane 1-1.csv"
 try_path = list_csv(try_path)
 
-# %%
+# %% Extract data from tpm csv file
 for file_path in try_path:
     with open(file_path) as f:
         reader = csv.reader(f)
         header_row = next(reader)
 
-        for index, column_header in enumerate(header_row):
-            print(index, column_header)
+        total_column, x_list, y_list = [], [], []
+        for column in reader:
+            aoi = int(column[1])
+            x_value = float(column[8])
+            y_value = float(column[9])
 
+            total_column.append(aoi)
+            x_list.append(x_value)
+            y_list.append(y_value)
 # %%
