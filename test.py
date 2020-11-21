0459267d758484e4e493c0a3271bfee469fe060a
LeoCCR <private@private.com>
2020-11-21 23:32:26 +0800

    Testing file.

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
+# %%
 import numpy as np
-import scipy as sp
+import numpy.ma as ma
 from random import randrange
 
-x = np.array([float(randrange(start=1, stop=5)) for _ in range(10)])
-y = np.array([float(randrange(start=1, stop=5)) for _ in range(10)])
+x = np.array([float(randrange(1, 5)) for _ in range(10)])
+y = np.array([float(randrange(1, 5)) for _ in range(10)])
+# %%
 x[x == 1] = np.nan
 y[y == 1] = np.nan
 
@@ -11,3 +13,11 @@ print(x)
 print(y)
 
 print(abs(np.ma.corrcoef(np.ma.masked_invalid(x), np.ma.masked_invalid(y))[0, 1]))
+
+# %%
+x = ma.masked_equal(x, 1)
+ma.count(x)
+np.sum(x)
+np.average(x)
+
+# %%
